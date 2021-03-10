dfc28e8a32369280c8093934c147f9779351a72c
dobneves <dobneves@mppr.mp.br>
2021-03-09 22:05:14 -0300

    ADD: Enron dataset processing script

---
+++ Projeto/process_enron.py
@@ -0,0 +1,96 @@
+import os
+from pathlib import Path
+from posix import listdir
+
+MAX_EMAILS_PER_PERSON = 100
+MAX_EMAILS = 4000
+
+# Verifica se o email não é encaminhado,
+# uma resposta a outro email ou contém outro
+# email no corpo
+def isEmailValid(content):
+  isForwarded = content.find('Forwarded') != -1
+  isReply = content.find('Original Message') != -1
+  hasOtherEmailInside = content.count('Subject: ') > 1
+  return not isForwarded and not isReply and not hasOtherEmailInside
+
+# Método usado para analisar a distribuição de emails
+# e encontrar um número máximo adequado de emails por
+# pessoa
+def countSentEmails(dir):
+  emailsPerPerson = {}
+  count = 0
+  for personName in dir:
+
+    try:
+      dir = os.listdir(f'./dataset/maildir/{personName}/sent')
+    except:
+      continue
+
+    emailsPerPerson[personName] = 0
+
+    for fileName in dir:
+
+      if (emailsPerPerson[personName] == MAX_EMAILS_PER_PERSON):
+        break
+
+      try:
+        file = open(f'./dataset/maildir/{personName}/sent/{fileName}')
+      except:
+        continue
+
+      try:
+        content = file.read()
+      except:
+        file.close()
+        continue
+
+      if isEmailValid(content):
+        count += 1
+        emailsPerPerson[personName] += 1
+
+      file.close()
+  return count, emailsPerPerson
+
+
+dir = os.listdir('./dataset/maildir')
+results = open('./dataset/benign_emails.txt', 'w')
+
+emailsPerPerson = {}
+totalCount = 0
+for personName in dir:
+
+  try:
+    dir = os.listdir(f'./dataset/maildir/{personName}/sent')
+  except:
+    continue
+
+  emailsPerPerson[personName] = 0
+
+  for fileName in dir:
+
+    if (emailsPerPerson[personName] == MAX_EMAILS_PER_PERSON):
+      break
+
+    if (totalCount == MAX_EMAILS):
+      results.close()
+      quit()
+
+    try:
+      file = open(f'./dataset/maildir/{personName}/sent/{fileName}')
+    except:
+      continue
+
+    try:
+      content = file.read()
+    except:
+      file.close()
+      continue
+
+    if isEmailValid(content):
+      results.write(content)
+      results.write('\n\n###---###---###---###\n\n')
+      totalCount += 1
+      emailsPerPerson[personName] += 1
+
+    file.close()
