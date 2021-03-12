330c12a8552c9f0bc5ecd7a0046284990cd45643
dobneves <dobneves@mppr.mp.br>
2021-03-12 01:39:24 -0300

    FIX: Percentage split

--- Projeto/percentage_split.py
+++ Projeto/percentage_split.py
@@ -17,14 +17,14 @@ def generateDataset():
     # Reads email datasets and creates arrays of objects
     # of type [subject, content_type, body, is_benign]
     try:
-        benign_dataset = readDataset('./dataset/benign_emails.txt', 1)
+        benign_dataset = readDataset('./dataset/raw/benign_emails.txt', 1)
     except:
         print('ERROR: Cannot find benign emails dataset')
         print('Please download and unzip the enron email dataset indo ./dataset')
         print('and run "python3 process_enron.py"')
 
     try:
-        fraud_dataset = readDataset('./dataset/fraudulent_emails.txt', 0)
+        fraud_dataset = readDataset('./dataset/raw/fraudulent_emails.txt', 0)
     except:
         print('ERROR: Cannot find fraudulent emails dataset, please download it')
         print('from kaggle and unzip it into ./dataset')
@@ -42,12 +42,12 @@ def getDataset80():
   # Tries to open existing dataset (saved as list)
   # if it does not exist, generates a new one
   try:
-      dataset_file = open('./dataset/dataset_80.bin', 'rb')
-      dataset = pickle.load(dataset_file)
+      dataset_file = open('./dataset/percentage-split/dataset_80.bin', 'rb')
   except:
-      dataset = generateDataset()
-      dataset_file = open('./dataset/dataset_80.bin', 'wb')
-      pickle.dump(dataset, dataset_file)
+      generateSplitDatasets()
+      dataset_file = open('./dataset/percentage-split/dataset_80.bin', 'wb')
+
+  dataset = pickle.load(dataset_file)
   return dataset
 
 
@@ -55,12 +55,12 @@ def getDataset20():
   # Tries to open existing dataset (saved as list)
   # if it does not exist, generates a new one
   try:
-      dataset_file = open('./dataset/dataset_80.bin', 'rb')
-      dataset = pickle.load(dataset_file)
+      dataset_file = open('./dataset/percentage-split/dataset_20.bin', 'rb')
   except:
-      dataset = generateDataset()
-      dataset_file = open('./dataset/dataset_80.bin', 'wb')
-      pickle.dump(dataset, dataset_file)
+      generateSplitDatasets()
+      dataset_file = open('./dataset/percentage-split/dataset_20.bin', 'wb')
+
+  dataset = pickle.load(dataset_file)
   return dataset
 
 
@@ -69,10 +69,14 @@ def generateSplitDatasets():
   dataset = generateDataset()
   splitIndex = int(round(0.8*len(dataset)))
   dataset_80, dataset_20 = dataset[:splitIndex], dataset[splitIndex:]
-  dataset_file = open('./dataset/dataset_80.bin', 'wb')
+
+  dataset_file = open('./dataset/percentage-split/dataset_80.bin', 'wb')
   pickle.dump(dataset_80, dataset_file)
-  dataset_file = open('./dataset/dataset_20.bin', 'wb')
+  dataset_file.close()
+
+  dataset_file = open('./dataset/percentage-split/dataset_20.bin', 'wb')
   pickle.dump(dataset_20, dataset_file)
+  dataset_file.close()
 
 
 generateSplitDatasets()
